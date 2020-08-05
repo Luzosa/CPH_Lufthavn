390daec8cd0257ed0880c402f70911091936440e
Leo Jensen <lion0005@zbc.dk>
2020-08-05 13:09:06 +0200

    Add project files.

---
+++ SQL/Select_Flights.sql
@@ -0,0 +1,4 @@
+﻿Select FlightCompanyName AS "Company", StartingLocation AS "Flight Departure", Destination AS "Flight Destination" From Destinations
+INNER JOIN Dest_Flight ON Destinations.DestinationID = Dest_Flight.Destination_FK 
+INNER JOIN Airplanes ON Dest_Flight.Airplanes_FK = Airplanes.AirplanesId
+INNER JOIN FlightCompany ON Airplanes.FlightCompany_FK = FlightCompany.FlightCompanyID;
